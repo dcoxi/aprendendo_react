2b6a03d2a0e3f5071a436eb956a0a204cd184594
Dilangue Pedro Coxi <d_pc1@hotmail.com>
2020-03-06 17:36:29 +0100

    consumir api do github no react

--- app/components/SearchUser.js
+++ app/components/SearchUser.js
@@ -1,10 +1,19 @@
 var React = require('react');
 var createReactClass = require('create-react-class');
+var GitHubUser = require('../services/GitHubUser');
 
 var SearchUser = createReactClass({
     handleSubmit: function(e) {
         e.preventDefault();   
         console.log(this.refs.username.value);
+
+        GitHubUser.getByUsername(this.refs.username.value).then(function(response){
+            console.log(response);
+        });
+        
+        GitHubUser.getReposByUsername(this.refs.username.value).then(function(response){
+            console.log(response);
+        });
     },
     render: function() {
         return (
